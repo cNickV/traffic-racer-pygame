56cae4cb7f80c8d838b85dea37451ad02aab1558
Nicolás Vega <nivemo035@hotmail.com>
2023-01-01 19:18:34 -0500

    se agregó coliccion a los vehiculos

--- trafficgame.py
+++ trafficgame.py
@@ -1,20 +1,19 @@
 import pygame
-
 from pygame.locals import *
+import random
 
 # Ajustar la resolución de la pantalla para que no se dibuje fuera de los límites
 size = width, height = (1200, 800)
 road_w = int(width / 1.6)
 roadmark_w = int(width/80)
+right_lane = width/2 + road_w/4
+left_lane = width/2 - road_w/4
 
 pygame.init()
 running = True
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("cNick's car game")
-
-# Rellenar el fondo de la pantalla con un color verde "limón"
-screen.fill((82, 190, 128))
 
 
 # Actualizar la pantalla para mostrar los cambios
@@ -23,27 +22,41 @@ pygame.display.update()
 # cargar vehículos
 car = pygame.image.load("car.png")
 car_loc = car.get_rect()
-car_loc.center = width/2 + road_w/4, height*0.8
+car_loc.center = right_lane, height*0.8
 
 # cargar enemigos
 
 otherCar = pygame.image.load("otherCar.png")
 otherCar_loc = otherCar.get_rect()
-otherCar_loc.center = width/2 - road_w/4, height*0.2
+otherCar_loc.center = left_lane, height*0.2
 
 
 # game loop
 while running:
 
+    # animar vehículo enemigo
+    otherCar_loc[1] += 3
+    if otherCar_loc[1] > height:
+        if random.randint(0, 1) == 0:
+            otherCar_loc.center = right_lane, -200
+        else:
+            otherCar_loc.center = left_lane, -200
+
+    # end game
+    if car_loc[0] == otherCar_loc[0] and otherCar_loc[1] > car_loc[1]:
+        print("GAME OVER!! PAPU, PERDISTE NOOB")
+        running = False
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
         if event.type == pygame.KEYDOWN:
             if event.key in [K_a, K_LEFT]:
                 car_loc = car_loc.move([-int(road_w/2), 0])
             if event.key in [K_d, K_RIGHT]:
                 car_loc = car_loc.move([int(road_w/2), 0])
+
+                # Rellenar el fondo de la pantalla con un color verde "limón"
+    screen.fill((82, 190, 128))
 
     # Dibujar las lineas de las carreteras
 
@@ -52,15 +65,15 @@ while running:
                      (width/2-road_w/2, 0, road_w, height))
 
     # lineas amarillas
-    pygame.draw.rect(screen, (241, 196, 15), (width/2 -
-                                              roadmark_w/2, 0, roadmark_w, height))
+    pygame.draw.rect(screen, (241, 196, 15),
+                     (width/2 - roadmark_w/2, 0, roadmark_w, height))
 
     # linea blanca izquierda
-    pygame.draw.rect(screen, (242, 243, 244), (width/2 -
-                                               road_w/2 + roadmark_w*2, 0, roadmark_w, height))
+    pygame.draw.rect(screen, (242, 243, 244),
+                     (width/2 - road_w/2 + roadmark_w*2, 0, roadmark_w, height))
     # linea blanca derecha
-    pygame.draw.rect(screen, (242, 243, 244), (width/2 +
-                                               road_w/2 - roadmark_w*3, 0, roadmark_w, height))
+    pygame.draw.rect(screen, (242, 243, 244),
+                     (width/2 + road_w/2 - roadmark_w*3, 0, roadmark_w, height))
 
     screen.blit(car, car_loc)
     screen.blit(otherCar, otherCar_loc)
